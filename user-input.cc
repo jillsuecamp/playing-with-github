439a762a8aa2de330845fa3062a5cbc9db170dfa
jilliancamp <jillsue@udel.edu>
2022-12-21 15:39:51 -0500

    third commit? maybe?

---
+++ user-input.cc
@@ -0,0 +1,15 @@
+#include <iostream> 
+
+
+using namespace std; 
+
+
+int main(){
+	string silly; 
+	cout << "Enter something silly!" << endl;
+	cin >> silly; 
+	cout << "You entered: " << silly; 
+}
+
+
+
